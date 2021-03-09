58228c65017fe4d10406245dd3ddfc033a6d5b60
Alexandre <amoutonbrady@gmail.com>
2021-03-09 22:09:34 +0100

    :sparkles: Adding favorite/unfavorite article

--- src/components/ArticlePreview.tsx
+++ src/components/ArticlePreview.tsx
@@ -1,40 +1,55 @@
 import { Link } from "solid-app-router";
-import { Component, Show } from "solid-js";
+import { Component, createState, Show } from "solid-js";
+
+import { useApi } from "../store/apiStore";
 import { useStore } from "../store/globalStore";
 import { IArticle } from "../types/article.interface";
+import { formatDate } from "../utils/formatDate";
 
 const ArticlePreview: Component<{ article: IArticle }> = (props) => {
   const [store] = useStore();
 
+  const api = useApi("article");
+  const [article, setArticle] = createState(props.article);
+
+  const updateFavorite = () => {
+    // optimistic UI
+    setArticle("favorited", (favorited) => !favorited);
+    setArticle("favoritesCount", (count) =>
+      article.favorited ? count + 1 : count - 1
+    );
+
+    const action = article.favorited ? "favorite" : "unfavorite";
+    return api[action](article.slug);
+  };
+
   return (
     <div class="article-preview">
       <div class="article-meta">
-        <Link href={`/profile/${props.article.author.username}`}>
-          <img src={props.article.author.image} />
+        <Link href={`/profile/${article.author.username}`}>
+          <img src={article.author.image} />
         </Link>
 
         <div class="info">
-          <Link
-            href={`/profile/${props.article.author.username}`}
-            class="author"
-          >
-            {props.article.author.username}
+          <Link href={`/profile/${article.author.username}`} class="author">
+            {article.author.username}
           </Link>
 
-          <span class="date">
-            {new Date(props.article.createdAt).toLocaleDateString()}
-          </span>
+          <span class="date">{formatDate(article.createdAt)}</span>
         </div>
         <Show when={store.isLoggedIn}>
-          <button class="btn btn-outline-primary btn-sm pull-xs-right">
-            <i class="ion-heart"></i> {props.article.favoritesCount}
+          <button
+            onClick={updateFavorite}
+            class="btn btn-outline-primary btn-sm pull-xs-right"
+          >
+            <i class="ion-heart"></i> {article.favoritesCount}
           </button>
         </Show>
       </div>
 
-      <Link href={`/article/${props.article.slug}`} class="preview-link">
-        <h1>{props.article.title}</h1>
-        <p>{props.article.description}</p>
+      <Link href={`/article/${article.slug}`} class="preview-link">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
         <span>Read more...</span>
       </Link>
     </div>
